5b29b684eac625ff2a561ed75e278000f034c6b1
Sumesh chandra jena <sumeshjena.co@gmail.com>
2021-09-26 10:40:00 +0530

    Added: Test calculated for Multiple Rides

--- src/test/java/cabInvoiceGenerator/InvoiceServiceTest.java
+++ src/test/java/cabInvoiceGenerator/InvoiceServiceTest.java
@@ -15,12 +15,31 @@ public class InvoiceServiceTest {
     }
 
     @Test
-    void givenLessDistanceAndTime_ThenShouldReturnMinimumFare() {
+    void givenLessDistanceAndLessTime_ThenShouldReturnLessFare() {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
-        double distance =0.1;
+        double distance = 1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(11, fare, 0.0);
+
+    }
+
+    @Test
+    void givenLessDistanceAndTime_ThenShouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
         int time = 1;
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assertions.assertEquals(5, fare, 0.0);
+    }
 
+    @Test
+    void givenMultipleRides_ThenShouldReturnInvoiceSummary() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1),
+        };
+        double fare = invoiceGenerator.calculateFare(rides);
+        Assertions.assertEquals(30, fare, 0.0);
     }
 }
